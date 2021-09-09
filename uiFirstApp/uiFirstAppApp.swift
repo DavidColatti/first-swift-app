7d7398a9912f24c90c783bbad67858559da0fd38
David Colatti <davidcolatti@gmail.com>
2021-09-09 10:18:28 -0400

    Initial Commit

---
+++ uiFirstApp/uiFirstAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  uiFirstAppApp.swift
+//  uiFirstApp
+//
+//  Created by David Colatti on 9/9/21.
+//
+
+import SwiftUI
+
+@main
+struct uiFirstAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
